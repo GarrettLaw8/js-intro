389d35e78d815350d78c56926019add1b023e148
Garrett Lawson <garrettlaw8@gmail.com>
2021-01-16 11:13:34 -0600

    variables/math

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 
-console.log("hello")
-let firstName = 'Garrett';
-let lastName = 'Lawson';
- 
+  // console.log("hello")
+  // let firstName = 'Garrett';
+  // let lastName = 'Lawson';
+  
 
-console.log (`${firstName} ${lastName}`)
+  // console.log (`${firstName} ${lastName}`)
+
+
+
+  // math 
+
+  // let cat = "cat"
+  // console.log(cat.length) // 3 ; 
